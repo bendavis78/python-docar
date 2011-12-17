127b4ae8374a9ec0c6e9b592df7b1343c53a3411
Christo Buschek <crito@30loops.net>
2011-12-17 18:44:46 +0100

    Initial commit.

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. django-documents documentation master file, created by
+   sphinx-quickstart on Sat Dec 17 18:44:13 2011.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to django-documents's documentation!
+============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+extra = {}
+requirements = [ 'distribute', 'docutils'],
+tests_require = ['nose', 'Mock==dev']
+
+if sys.version_info <= (2,6):
+    requirements.append('simplejson')
+
+setup(
+    name = "documents",
+    version = "0.1",
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    #include_package_data=True,
+    zip_safe=False, # Don't create egg files, Django cannot find templates in egg files.
+    install_requires = requirements,
+
+    tests_require=tests_require,
+    setup_requires='nose',
+    test_suite = "nose.collector",
+    extras_require={'test': tests_require},
+
+    author = "Christo Buschek",
+    author_email = "crito@30loops.net",
+    url = "https://github.com/30loops/django-documents",
+    description = "Create document oriented RESTful services for client and \
+server applications.",
+    long_description = read('README.rst'),
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    **extra
+)
