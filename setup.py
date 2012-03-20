67f4e8d2868a82f28fd825d3d7332425242c2194
Christo Buschek <crito@30loops.net>
2012-03-20 14:14:21 +0100

    Bumped version to 0.5.1.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if sys.version_info <= (2, 6):
 
 setup(
     name="docar",
-    version="0.5",
+    version="0.5.1",
     packages=['docar', 'docar.backends'],
     include_package_data=True,
     #zip_safe=False,  # Don't create egg files, Django cannot find templates
